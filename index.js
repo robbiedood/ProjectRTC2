2ce894ea6f9d8fcbb25b40d3da47d791667f7933
lukelu0520 <appl.chlu@gmail.com>
2020-11-30 12:51:29 -0800

    set listening port to 3000

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var socketIO = require('socket.io');
 var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
-}).listen(8080);
+}).listen(3000);
 
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
